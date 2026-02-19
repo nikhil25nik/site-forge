d434fdcf4d9cbd3d05654146be78bd19dd387f45
nikhil25nik <nikhilkurane25@gmail.com>
2026-02-19 10:57:55 +0530

    chnages in server.ts

--- server/server.ts
+++ server/server.ts
@@ -17,6 +17,8 @@ const corsOptions = {
     credentials:true
 }
 app.use(cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
+
 app.post("/api/stripe",express.raw({type:"application/json"}),stripeWebhooks)
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
